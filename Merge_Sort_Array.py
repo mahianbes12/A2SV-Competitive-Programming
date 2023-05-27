9a26c5cd25d61d950b1c2c9d1585b604a52e4aa4
mahianbes12 <88880755+mahianbes12@users.noreply.github.com>
2023-05-27 20:44:56 +0300

    Merge_Sorted_Array.py

--- Merge_Sort_Array.py
+++ Merge_Sort_Array.py
@@ -1,5 +1,5 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-      for j in range(n):
-          nums1[m+j] = nums2[j]
+      for i in range(n):
+          nums1[m+i] = nums2[i]
       nums1.sort()
