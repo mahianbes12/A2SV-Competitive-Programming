e3fb3fdb4717aa742d01c8be4aefbfee8d6785c8
mahianbes12 <88880755+mahianbes12@users.noreply.github.com>
2023-05-30 21:15:12 +0300

    isValid.py

---
+++ isValid.py
@@ -0,0 +1,13 @@
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        res, stack = [], [(root, False)]
+        while stack:
+            node, visited = stack.pop()  # the last element
+            if node:
+                if visited:  
+                    res.append(node.val)
+                else:  # preorder: root -> left -> right
+                    stack.append((node.right, False))
+                    stack.append((node.left, False))
+                    stack.append((node, True))
+        return res
